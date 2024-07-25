0892cab95d91e1dbc02d7be3a3bd9da7c81f6f94
jigovind556 <jigovind556@gmail.com>
2024-07-26 02:22:07 +0530

    configured mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,15 @@
-//hello
+require("dotenv").config();
+const { default: mongoose } = require("mongoose");
+const { DB_NAME } = require("./constants");
+const { connectDB } = require("./db/index.js");
+const { app } = require("./app.js");
+const port = process.env.PORT || 3000;
+
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => console.log("MongoDB connection Failed !!! ", err));
